713e0328b610356342037932e3762e188fb49083
TulioliAles <desmecad@hotmail.com>
2020-07-03 16:56:35 -0300

    Commit 3 - Finalização: Criado Form para novos posts, inserida mensagem de validação e inserido controle de segurança quando acionar o botão de novo post

--- src/main/java/com/br/codeblog/controller/CodeblogController.java
+++ src/main/java/com/br/codeblog/controller/CodeblogController.java
@@ -1,13 +1,18 @@
 package com.br.codeblog.controller;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.br.codeblog.model.Post;
 import com.br.codeblog.service.CodeblogService;
@@ -34,4 +39,20 @@ public class CodeblogController {
 		return mv;
 	}
 	
+	@RequestMapping(value = "/newpost", method = RequestMethod.GET)
+	public String getPostForm() {
+		return "postForm";
+	}
+	
+	@RequestMapping(value = "/newpost", method = RequestMethod.POST)
+	public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+		if(result.hasErrors()) {
+			attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos.");
+			return "redirect:/newpost";
+		}
+		post.setData(LocalDate.now());
+		codeblogService.save(post);
+		return "redirect:/posts";
+	}
+
 }
